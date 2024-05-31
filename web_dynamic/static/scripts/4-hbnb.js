9f345942afdbab4f2490dbfc16832c969c29f7b6
YASSINE <contact@yassine.fun>
2024-05-31 08:17:02 -0700

    Add amenity filter functionality

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    const amenities = {};
+  
+    $('input[type="checkbox"]').change(function () {
+      if ($(this).is(':checked')) {
+        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete amenities[$(this).attr('data-id')];
+      }
+      $('.amenities h4').text(Object.values(amenities).join(', '));
+    });
+  
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+      if (textStatus === 'success' && data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    });
+  
+    $('button').click(function () {
+      $.ajax({
+        url: 'http://0.0.0.0:5001/api/v1/places_search/',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ amenities: Object.keys(amenities) }),
+        success: function (data) {
+          $('.places').empty();
+          for (const place of data) {
+            const article = `
+              <article>
+                <div class="title_box">
+                  <h2>${place.name}</h2>
+                  <div class="price_by_night">$${place.price_by_night}</div>
+                </div>
+                <div class="information">
+                  <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+                  <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+                  <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+                </div>
+                <div class="description">
+                  ${place.description}
+                </div>
+              </article>
+            `;
+            $('.places').append(article);
+          }
+        }
+      });
+    });
+  });
